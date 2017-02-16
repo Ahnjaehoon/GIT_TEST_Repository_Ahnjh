ee50ba25b01203d5fa524a70bb45f1edfb0027be
Ahnjaehoon <ahnjh@te.com>
2017-02-16 16:08:26 +0900

    Rev.13 : User Area 분리

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ main()
     {
         while ( u8s_T_Main != big_SET ){} /* 10ms Polling Wait Time */
         u8s_T_Main = big_RESET; 
+        /* Ahnjh Area *******************************/
         TEK_func1();
         TEK_func2();
         TEK_func3();
@@ -17,6 +18,10 @@ main()
         TEK_func_Temp1();
         TEK_func_Temp2();
         TEK_func_Temp3();
+        /*********************************************/
+        
+        /* USER2 Area *******************************/
+        /*********************************************/
     }
     
     return;
